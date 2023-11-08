76f3ac61667e52e214190423828a551b3d34f547
Ryuivan <ryu.ivan@student.umn.ac.id>
2023-11-08 15:05:15 +0700

    8/11/2023 15:05

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -4,8 +4,16 @@ import { Row, Col } from "react-bootstrap";
 export default function News() {
   return (
     <div className="news-bg">
-      <img src="https://cdn.discordapp.com/attachments/1139426470901600336/1162958206755164192/left.webp" alt="left-ornament" className="left-news" />
-      <img src="https://cdn.discordapp.com/attachments/1139426470901600336/1162958207002611752/right.webp" alt="right-ornament" className="right-news" />
+      <img
+        src="https://cdn.discordapp.com/attachments/1139426470901600336/1162958206755164192/left.webp"
+        alt="left-ornament"
+        className="left-news"
+      />
+      <img
+        src="https://cdn.discordapp.com/attachments/1139426470901600336/1162958207002611752/right.webp"
+        alt="right-ornament"
+        className="right-news"
+      />
       <Row className="news-container m-0 gx-5">
         <Col lg={6} className="d-flex align-items-center">
           <img
@@ -17,10 +25,10 @@ export default function News() {
         <Col lg={6} className="d-flex align-items-center">
           <div className="news-desc">
             <p>
-              Discover the latest event updates here! Stay informed about the
-              event schedule, featured guests, and special activities. Check
-              back regularly for all the exciting news and behind-the-scenes
-              insights. Don't miss a thing!
+              Workout Bareng Yuk! Workshop Workout ini akan dipandu oleh Coach
+              Nisa yang merupakan APKI Certified Fitness Trainer. Terlebih lagi
+              kalian bisa mendapatkan cek body composition gratis dan +1 SKKM
+              Minat dan Bakat serta ada hadiah menarik bagi yang beruntung.
               <img
                 src="https://cdn.discordapp.com/attachments/1139426470901600336/1155555480853295114/newsStar.webp"
                 alt="news-star"
